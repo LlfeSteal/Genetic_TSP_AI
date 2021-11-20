01374d46a1f16e8f195d392e07e4f350d6309041
LifeSteal <166237187+LlfeSteal@users.noreply.github.com>
2021-11-20 16:32:16 +0100

    [ADD] Ajout de la classe CityDistance

---
+++ entities/CityDistance.py
@@ -0,0 +1,10 @@
+class CityDistance:
+    def __init__(self, city, distance):
+        self._city = city
+        self._distance = distance
+
+    def get_city(self):
+        return self._city
+
+    def get_distance(self):
+        return self._distance
