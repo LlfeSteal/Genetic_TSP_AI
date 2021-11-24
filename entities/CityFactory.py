54c5a635d19aea77ea42fde8da1e4b756793a4df
dorianosaure <contact.dorianosaure@gmail.com>
2021-11-24 15:32:00 +0100

    [FIX] Correction d'un bug lié aux tableau de villes dans la methode generate_city_distances de la class CityFactory

--- entities/CityFactory.py
+++ entities/CityFactory.py
@@ -1,6 +1,6 @@
 import random
 
-from City import City
+from entities.City import City
 from entities.CityDistance import CityDistance
 
 
@@ -17,11 +17,11 @@ class CityFactory:
     @staticmethod
     def generate_city_distances(cities):
         cities_set = cities[:]
-        cities_set.remove(cities_set.getItem(0))
+        cities_set.remove(cities_set[0])
         for first_city in cities:
             for second_city in cities_set:
                 distance = random.randint(0, 1000)
                 first_city.add_distance(CityDistance(second_city, distance))
                 second_city.add_distance(CityDistance(first_city, distance))
-        cities_set.remove(cities_set.getItem(0))
+        cities_set.remove(cities_set[0])
 
