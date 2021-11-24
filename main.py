af58b164acff21f2b85ec693b07dac0e53a9c1fe
LifeSteal <166237187+LlfeSteal@users.noreply.github.com>
2021-11-24 15:38:14 +0100

    [UPDATE] Generation de 25 populations et affichage du resultat dans la console

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-# This is a sample Python script.
-
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import sys
+sys.path.append("C:/Users/dorian/PycharmProjects/genetictsp_ia")
+from GeneticTSP import GeneticTSP
 
 
 def print_hi(name):
@@ -11,6 +10,9 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
 
+    genetic_tsp = GeneticTSP()
+    for i in range(25):
+        genetic_tsp.generate_new_population()
+    genetic_tsp.display_population_status()
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
