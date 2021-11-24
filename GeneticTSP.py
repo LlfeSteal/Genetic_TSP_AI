39eb387a19c1cb05e19b4e77cec3e9c326da6b06
dorianosaure <contact.dorianosaure@gmail.com>
2021-11-24 13:57:46 +0100

    [UPDATE] Ajout de la methode generate_new_childs dans la class GeneticTSP.py

--- GeneticTSP.py
+++ GeneticTSP.py
@@ -4,6 +4,7 @@ from entities.CityManager import CityManager
 from entities.Subject import Subject
 from entities.SubjectCalculator import SubjectCalculator
 from entities.SubjectScoreManager import SubjectScoreManager
+import numpy as np
 
 
 class GeneticTSP:
@@ -22,13 +23,16 @@ class GeneticTSP:
         return random.shuffle(self.city_manager.get_cities())
 
     def generate_new_population(self):
+        new_generation = []
         best_subjects = SubjectCalculator.get_best_subjects(self.population)
         subject_score_manager = SubjectScoreManager(best_subjects)
-        for i in range(int(self.population_to_cross/2)):
+        for i in range(int(self.population_to_cross / 2)):
             subject1 = subject_score_manager.pick_subject()
             subject2 = subject_score_manager.pick_subject()
 
-
-
-
-
+    @staticmethod
+    def generate_new_childs(subject1, subject2):
+        cross_index = random.randint(3, 7)
+        child1 = np.append(subject1[:cross_index], subject2[cross_index:])
+        child2 = np.append(subject2[:cross_index], subject1[cross_index:])
+        return [child1, child2]
