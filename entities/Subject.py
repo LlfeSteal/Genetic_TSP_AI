2c89fa0343b0b2f335abb5cb774f7fbdcc7cdbed
LifeSteal <166237187+LlfeSteal@users.noreply.github.com>
2021-11-24 15:59:28 +0100

    [UPDATE] Ajout de la methode __str__ dans la classe Subject

--- entities/Subject.py
+++ entities/Subject.py
@@ -5,3 +5,9 @@ class Subject:
     def get_cities(self):
         return self._cities
 
+    def __str__(self):
+        cities_names = []
+        for city in self._cities:
+            cities_names.append(str(city))
+        return str(cities_names)
+
