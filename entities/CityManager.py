3abe86fc0ab4dcacf59eb7b76292a3d95195a066
dorianosaure <contact.dorianosaure@gmail.com>
2021-12-07 00:25:42 +0100

    [UPDATE] Ajout du nombre de ville à gérer dans la classe CityManager

--- entities/CityManager.py
+++ entities/CityManager.py
@@ -2,9 +2,9 @@ from entities.CityFactory import CityFactory
 
 
 class CityManager:
-    def __init__(self, max_distance):
+    def __init__(self, max_distance, city_number):
         self.city_factory = CityFactory(max_distance)
-        self._cities = self.city_factory.init_cities(13)
+        self._cities = self.city_factory.init_cities(city_number)
 
     def get_cities(self):
         return self._cities
