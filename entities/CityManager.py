56c7ad5f787817b8cc0256f10ecaf5f08498f8e3
LifeSteal <166237187+LlfeSteal@users.noreply.github.com>
2021-12-06 17:18:50 +0100

    [UPDATE] Instanciation de la classe CityFactory dans la classe CityManager pour utiliser les nouvelles methodes

--- entities/CityManager.py
+++ entities/CityManager.py
@@ -3,7 +3,8 @@ from entities.CityFactory import CityFactory
 
 class CityManager:
     def __init__(self):
-        self._cities = CityFactory.init_cities(13)
+        self.city_factory = CityFactory(400)
+        self._cities = self.city_factory.init_cities(13)
 
     def get_cities(self):
         return self._cities
