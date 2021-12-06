8e395f735a5b21a512d09ec9a71b64cb3cfc1893
dorianosaure <contact.dorianosaure@gmail.com>
2021-12-06 17:10:40 +0100

    [UPDATE] Ajout de de la methode get_distance_between_two_cities dans la classe CityFactory

--- entities/CityFactory.py
+++ entities/CityFactory.py
@@ -1,3 +1,4 @@
+import math
 import random
 
 from entities.City import City
@@ -30,6 +31,10 @@ class CityFactory:
         random_x = random.randint(0, self._max_distance)
         random_y = random.randint(0, self._max_distance)
         return random_x, random_y
+
+    def get_distance_between_two_cities(self, city1, city2):
+        dist = math.sqrt((city2.get_x() - city1.get_x()) ** 2 + (city2.get_y() - city1.get_y()) ** 2)
+        return dist
 
 
 
