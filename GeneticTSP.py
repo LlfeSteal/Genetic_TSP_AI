d85a60353d3b24ffd80d6ac1aabc6f35caa09eb3
dorianosaure <contact.dorianosaure@gmail.com>
2021-12-07 00:25:18 +0100

    [FIX] Correction d'un bug qui ne prenait pas en compte la nouvelle génération

--- GeneticTSP.py
+++ GeneticTSP.py
@@ -9,11 +9,12 @@ import numpy as np
 
 
 class GeneticTSP:
-    def __init__(self, population_number=40, population_to_cross=30, max_distance=900, mutation_rate=0.05):
-        self.population_number = population_number
+    def __init__(self, population_number=40, population_to_cross=30, max_distance=900, mutation_rate=0.05, city_number=13):
+        self.city_number = city_number
         self.population_to_cross = population_to_cross
+        self.population_number = population_number
         self.mutation_rate = mutation_rate
-        self.city_manager = CityManager(max_distance)
+        self.city_manager = CityManager(max_distance, city_number)
         self.population = []
         self.generate_population()
         self.graph_generator = GraphGenerator(self.city_manager.get_cities(), max_distance)
@@ -35,14 +36,16 @@ class GeneticTSP:
         for i in range(int(self.population_to_cross / 2)):
             subject1 = subject_score_manager.pick_subject()
             subject2 = subject_score_manager.pick_subject()
-            np.append(new_generation, self.generate_new_childs(subject1, subject2))
+            childs = self.generate_new_childs(subject1, subject2)
+            new_generation = np.append(new_generation, childs)
         missing_subject_number = self.population_number - len(new_generation)
         for i in range(missing_subject_number):
-            new_generation.append(best_subjects[i].get_subject())
+            new_generation = np.append(new_generation, best_subjects[i].get_subject())
         self.population = new_generation
 
     def generate_new_childs(self, subject1, subject2):
-        cross_index = random.randint(3, 7)
+        # cross_index = random.randint(2, self.city_number-2)
+        cross_index = 4
         subject1_cities = subject1.get_cities()
         subject2_citites = subject2.get_cities()
         child1 = np.append(subject1_cities[:cross_index], subject2_citites[cross_index:])
@@ -71,9 +74,9 @@ class GeneticTSP:
     def get_graph_generator(self):
         return self.graph_generator
 
-    def display_graph(self):
+    def display_graph(self, subject):
         self.graph_generator.create_graph()
-        self.graph_generator.display()
+        self.graph_generator.display(subject)
 
     def roll_mutation(self, subject):
         rand = random.uniform(0.0, 100.0)
