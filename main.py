daf35a165b321e90326eb3bd68ed171b1ea35dc7
LifeSteal <166237187+LlfeSteal@users.noreply.github.com>
2021-12-06 23:17:05 +0100

    [FIX] Correction de l'affichage console

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import sys
 
+from entities.SubjectCalculator import SubjectCalculator
+
 sys.path.append("C:/Users/dorian/PycharmProjects/genetictsp_ia")
 from GeneticTSP import GeneticTSP
 
@@ -12,12 +14,13 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
 
-    genetic_tsp = GeneticTSP(200, 1000, 30)
+    genetic_tsp = GeneticTSP(300, 292, 30, 0.08)
     for i in range(100):
+        print("generation : ", i)
         genetic_tsp.generate_new_population()
     genetic_tsp.display_population_status()
-    print("best : ", str(genetic_tsp.get_best_subjects(1)))
     print("-----------------------------------------------------------")
     genetic_tsp.display_population_score()
+    print("best : ", str(genetic_tsp.get_best_subjects(1)), SubjectCalculator.evaluate(genetic_tsp.get_best_subjects(1)))
     genetic_tsp.display_graph()
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
