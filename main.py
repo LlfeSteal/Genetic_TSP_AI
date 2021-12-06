3d3dc95233d035de8bcc8bcb0ba3ff2448a60a3d
dorianosaure <contact.dorianosaure@gmail.com>
2021-12-06 22:01:53 +0100

    [UPDATE] Ajout de l'affichage du graph dans le main

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+
 sys.path.append("C:/Users/dorian/PycharmProjects/genetictsp_ia")
 from GeneticTSP import GeneticTSP
 
@@ -11,9 +12,10 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
 
-    genetic_tsp = GeneticTSP()
-    for i in range(25):
+    genetic_tsp = GeneticTSP(200, 1000, 30)
+    for i in range(100):
         genetic_tsp.generate_new_population()
     genetic_tsp.display_population_status()
     print("best : ", str(genetic_tsp.get_best_subjects(1)))
+    genetic_tsp.display_graph()
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
