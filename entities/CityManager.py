9f34590c7755f2a706ac0f2ff3c784b59e64a06b
dorianosaure <contact.dorianosaure@gmail.com>
2021-12-06 21:02:33 +0100

    [UPDATE] Ajout de la max distance dans la classe CityManager

--- entities/CityManager.py
+++ entities/CityManager.py
@@ -2,8 +2,8 @@ from entities.CityFactory import CityFactory
 
 
 class CityManager:
-    def __init__(self):
-        self.city_factory = CityFactory(400)
+    def __init__(self, max_distance):
+        self.city_factory = CityFactory(max_distance)
         self._cities = self.city_factory.init_cities(13)
 
     def get_cities(self):
