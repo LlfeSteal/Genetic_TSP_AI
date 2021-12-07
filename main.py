d3b881c9ca89ef95ff381f4f93347e66cf5b6a0d
dorianosaure <contact.dorianosaure@gmail.com>
2021-12-07 01:13:19 +0100

    [UDPATE] Mise à jour du main pour afficher le graph

--- main.py
+++ main.py
@@ -14,13 +14,17 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
 
-    genetic_tsp = GeneticTSP(300, 292, 30, 0.08)
+    genetic_tsp = GeneticTSP(500, 498, 30, 0.1, 10)
     for i in range(100):
         print("generation : ", i)
         genetic_tsp.generate_new_population()
+        print("best : ", str(genetic_tsp.get_best_subjects(1)),
+              SubjectCalculator.evaluate(genetic_tsp.get_best_subjects(1)))
+        genetic_tsp.display_graph(genetic_tsp.get_best_subjects(1))
     genetic_tsp.display_population_status()
     print("-----------------------------------------------------------")
     genetic_tsp.display_population_score()
-    print("best : ", str(genetic_tsp.get_best_subjects(1)), SubjectCalculator.evaluate(genetic_tsp.get_best_subjects(1)))
-    genetic_tsp.display_graph()
+    print("best : ", str(genetic_tsp.get_best_subjects(1)),
+          SubjectCalculator.evaluate(genetic_tsp.get_best_subjects(1)))
+    genetic_tsp.display_graph(genetic_tsp.get_best_subjects(1))
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
