82596f1de03c1eea8b0744bea2e1474b43f50ec2
dorianosaure <contact.dorianosaure@gmail.com>
2021-12-13 02:40:56 +0100

    [UDPATE] Ajout de la sauvegarde d'était des villes

--- entities/CityManager.py
+++ entities/CityManager.py
@@ -1,12 +1,16 @@
+import pickle
 import random
 
 from entities.CityFactory import CityFactory
 
 
 class CityManager:
-    def __init__(self, max_distance, city_number):
+    def __init__(self, max_distance, city_number, save=None):
         self.city_factory = CityFactory(max_distance)
-        self._cities = self.city_factory.init_cities(city_number)
+        if save is not None:
+            self.load(save)
+        else:
+            self._cities = self.city_factory.init_cities(city_number)
 
     def get_cities(self):
         return self._cities
@@ -18,3 +22,13 @@ class CityManager:
                 missing_cities.append(city)
         random.shuffle(missing_cities)
         return missing_cities
+
+    def save(self, name):
+        with open('saved_state_'+name, 'wb') as file:
+            pickle.dump(self._cities, file)
+
+    def load(self, name):
+        print("load")
+        self._cities = []
+        with open('saved_state_'+name, 'rb') as file:
+            self._cities = pickle.load(file)
--- main.py
+++ main.py
@@ -14,17 +14,22 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
 
-    genetic_tsp = GeneticTSP(500, 498, 30, 0.1, 10)
-    for i in range(100):
-        print("generation : ", i)
-        genetic_tsp.generate_new_population()
-        print("best : ", str(genetic_tsp.get_best_subjects(1)),
-              SubjectCalculator.evaluate(genetic_tsp.get_best_subjects(1)))
+    sum = 0
+    for y in range(100):
+        print("test : ", y)
+        genetic_tsp = GeneticTSP(50, 50, 30, 1, 13, "test")
+        for i in range(100):
+            #print("generation : ", i)
+            genetic_tsp.generate_new_population()
+            #print("best : ", str(genetic_tsp.get_best_subjects(1)),
+                  #SubjectCalculator.evaluate(genetic_tsp.get_best_subjects(1)))
+        genetic_tsp.display_population_status()
+        #print("-----------------------------------------------------------")
+        genetic_tsp.display_population_score()
+        best_score = SubjectCalculator.evaluate(genetic_tsp.get_best_subjects(1))
+        #print("best : ", str(genetic_tsp.get_best_subjects(1)),
+              #best_score)
+        sum += best_score
         genetic_tsp.display_graph(genetic_tsp.get_best_subjects(1))
-    genetic_tsp.display_population_status()
-    print("-----------------------------------------------------------")
-    genetic_tsp.display_population_score()
-    print("best : ", str(genetic_tsp.get_best_subjects(1)),
-          SubjectCalculator.evaluate(genetic_tsp.get_best_subjects(1)))
-    genetic_tsp.display_graph(genetic_tsp.get_best_subjects(1))
+    print("sum test : ", sum)
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
