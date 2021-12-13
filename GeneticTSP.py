44cd925dfa21b41380d683301bddccb2acb36e83
LifeSteal <166237187+LlfeSteal@users.noreply.github.com>
2021-12-13 02:40:56 +0100

    [UDPATE] Ajout de la sauvegarde d'était des villes

--- GeneticTSP.py
+++ GeneticTSP.py
@@ -9,13 +9,13 @@ import numpy as np
 
 
 class GeneticTSP:
-    def __init__(self, population_number=40, population_to_cross=30, max_distance=900, mutation_rate=0.05,
-                 city_number=13):
+    def __init__(self, population_number=40, population_to_cross=30, max_distance=900, mutation_rate=5,
+                 city_number=13, save=None):
         self.city_number = city_number
         self.population_to_cross = population_to_cross
         self.population_number = population_number
         self.mutation_rate = mutation_rate
-        self.city_manager = CityManager(max_distance, city_number)
+        self.city_manager = CityManager(max_distance, city_number, save)
         self.population = []
         self.generate_population()
         self.graph_generator = GraphGenerator(self.city_manager.get_cities(), max_distance)
@@ -39,9 +39,11 @@ class GeneticTSP:
             subject2 = subject_score_manager.pick_subject()
             childs = self.generate_new_childs(subject1, subject2)
             new_generation = np.append(new_generation, childs)
+        new_generation = np.append(new_generation, best_subjects[0].get_subject())
         missing_subject_number = self.population_number - len(new_generation)
         for i in range(missing_subject_number):
-            new_generation = np.append(new_generation, best_subjects[i].get_subject())
+            #new_generation = np.append(new_generation, best_subjects[i].get_subject())
+            new_generation = np.append(new_generation, subject_score_manager.pick_subject())
         self.population = new_generation
 
     def generate_new_childs(self, subject1, subject2):
@@ -53,9 +55,8 @@ class GeneticTSP:
         return [self.roll_mutation(Subject(child1)), self.roll_mutation(Subject(child2))]
 
     def cross_childs(self, child1, child2):
-        cross_index = 4
-        new_child = []
-        new_child = np.append(new_child, child1[:cross_index])
+        cross_index = random.randint(0, self.city_number - 1)
+        new_child = child1.tolist()[:cross_index]
         for city in child2[cross_index:]:
             if city not in new_child:
                 new_child = np.append(new_child, city)
@@ -63,6 +64,13 @@ class GeneticTSP:
         new_child = np.append(new_child, missing_cities)
 
         return new_child
+
+    @staticmethod
+    def display_child(child):
+        cities = []
+        for city in child:
+            cities.append(city.get_name())
+        print(cities)
 
     def display_population_status(self):
         for subject in self.population:
